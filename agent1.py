6d2ac11a81201f8ecee97d0a05c900647578feb5
max <max_kirk1@icloud.com>
2025-11-25 15:36:05 +0000

    initial commit

---
+++ agent1.py
@@ -0,0 +1,23 @@
+import random
+from extension.board_utils import list_legal_moves_for, take_notes
+
+def agent(board, player, var):
+    piece, move_opt = None, None
+    print(f"Ply: {var[0]}")
+    
+    if player.name == "white":
+        legal = list_legal_moves_for(board, player)
+        if legal:
+            # Randome choice from a list of legal move from all pieces
+            piece, move_opt = random.choice(legal)
+    else:
+        legal = list_legal_moves_for(board, player)
+        if legal:
+            # Randome choice from a list of legal move from all pieces
+            piece, move_opt = random.choice(legal)
+
+    return piece, move_opt
+
+
+def Eval():
+    pass
