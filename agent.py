a6ab5867ddf7e4ce9c001f3994a00582863f8b00
max <max_kirk1@icloud.com>
2025-11-25 15:45:20 +0000

    modified:   agent.py 	deleted:    agent1.py 	new file:   agent_eg.py

--- agent.py
+++ agent.py
@@ -2,31 +2,6 @@ import random
 from extension.board_utils import list_legal_moves_for, take_notes
 
 def agent(board, player, var):
-    """"
-    This is an example of your designed Agent
-
-    Parameters
-    ----------
-    board: the current chess board
-    player: your assigned player role (white or black)
-    var:  [ply, THINKING_TIME_BUDGET] a list cotaining the ply ID and the thinking_time_budget (secs)
-
-    Returns
-    -------
-    piece: your selected chess piece
-    move_opt: your selected move of your selected chess piece
-    
-    Hints:
-    -----
-    - List of players on the current board game: list(board.players) - default list: [Player (white), Player (black)]
-    - board.players[0].name = "white" and board.players[1].name = "black"
-    - Name of the player assigned to the Agent (either "white" or "black"): player.name
-    - list of pieces of the current player: list(board.get_player_pieces(player))
-    - List of pieces and corresponding moves for each pieces of the player: piece, move_opt = list_legal_moves_for(board, player)
-    - From var: ply ID = var[0], timeout = var[1]
-    - Use the timeout variable together with time.perf_counter()
-    to ensure the agent returns its best move before the time limit expires.
-    """
     piece, move_opt = None, None
     print(f"Ply: {var[0]}")
     
@@ -42,3 +17,7 @@ def agent(board, player, var):
             piece, move_opt = random.choice(legal)
 
     return piece, move_opt
+
+
+def Eval():
+    pass
