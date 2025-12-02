55a807f987456b853fe343a35c10ee3830832090
max <max_kirk1@icloud.com>
2025-12-02 15:48:29 +0000

    intialisation of node set up, including zobrist keys and class methods

--- node.py
+++ node.py
@@ -2,164 +2,132 @@ from math import inf
 from random import getrandbits
 from extension.board_rules import get_result, cannot_move
 from extension.board_utils import list_legal_moves_for, copy_piece_move, take_notes
+from chessmaker.chess.base import Board, Piece, MoveOption, Square
 
 
 class Node:
-    """represents a single state in the game tree"""
+    """represent single state in game tree"""
 
-    ZOBRIST_KEYS: dict[str, int] = {}
-    transposition_table: dict[int, 'Node'] = {}  # maps board signature to nodes
+    _zobrist_keys: dict[str, int] = {}
+    transposition_table: dict[int, "Node"] = {}  # map board z_hash to node
+    _RANDBITS: int = 64
 
-    def __init__(self, board, parent=None, move=None, signature: int = None):
-        """initialise a new node -> non-automatic expansion"""
-        if not Node.ZOBRIST_KEYS:
-            self._initialise_zobrist_keys()
-        self.board = board
-        # same board state can come from many paths so possibly multiple parents
-        self.parent = [] if parent is None else [parent]
-        self.move = move
-        self.children = []
-        self.value = None
-        self.depth = 0 if parent is None else parent.depth + 1
-        self.board_signature = signature if signature is not None else self._calculate_zobrist_signature(self.board)
-        if self.board_signature not in self.transposition_table:
-            self.transposition_table[self.board_signature] = self
-
-        if self.is_terminal():  # do not continue to recurse if terminal
-            if cannot_move(self.board):
-                self.value = inf  # winning move
-            else:
-                self.value = -inf  # loosing move
-
-    def is_terminal(self) -> bool:
-        """checks current board state is not terminal"""
-        result = get_result(self.board)
-        return result is not None
-
-    def expand(self, max_depth):
-        """expand legal moves and create children that aren't terminal, up to depth
-        and hasn't already been expanded"""
-
-        # TODO sort each time in order of highest value and expand that node first if winning state stop recurse
-        # if has children (already expanded) or reached max depth don't expand
-        if self.children or self.depth >= max_depth:
-            return
-
-        player_to_expand = self.board.current_player
-        moves_to_expand = list_legal_moves_for(self.board, player_to_expand)
-        for piece, move in moves_to_expand:
-            new_board = execute_move_onboard(self.board, piece, move)
-            if new_board:
-                child_signature = self._calculate_incremental_zobrist(self, piece, move, new_board)
-                # if child's signature already exists link and dont create new node
-                if child_signature in self.transposition_table:
-                    existing_child = self.transposition_table[child_signature]
-                    if self not in existing_child.parent:
-                        existing_child.parent.append(self)
-                    self.children.append(existing_child)
-                    continue
-                child = Node(new_board, parent=self, move=(piece, move), signature= child_signature)
-                self.children.append(child)
-
+    # todo add en-peasant keys to class methods
     @classmethod
-    def _get_piece_key(cls, piece) -> str:
-        """helper to generate consistent key for piece"""
-        pos = piece.position
-        return f"{piece.player.name.lower()}_{piece.name}_{pos.x}_{pos.y}"
-
-    @classmethod
-    def _calculate_zobrist_signature(cls, board):
-        """xor each of the keys to get zobrist hash of board"""
-        zobrist_hash = 0
-        for piece in board.get_pieces():
-            key = cls._get_piece_key(piece)
-            try:
-                zobrist_hash ^= cls.ZOBRIST_KEYS[key]
-            except KeyError:
-                take_notes(f"missing key for {key}")
-        turn_key = f"turn_{board.current_player.name.lower()}"
-        zobrist_hash ^= cls.ZOBRIST_KEYS[turn_key]
-        # todo keys for en-peasant
-
-        return zobrist_hash
-
-    @classmethod
-    def _calculate_incremental_zobrist(cls, parent: 'Node', piece_to_move, move_option, new_board):
-        current_hash = parent.board_signature
-        # xor out old state
-        old_piece_key = cls._get_piece_key(piece_to_move)
-        current_hash ^= cls.ZOBRIST_KEYS[old_piece_key]
-
-        # remove captured pieces
-        for capture_pos in move_option.captures:
-            captured_piece_details = find_piece_at_position(parent.board, capture_pos)
-            if captured_piece_details:
-                captured_key = cls._get_piece_key(captured_piece_details)
-                current_hash ^= cls.ZOBRIST_KEYS[captured_key]
-            else:
-                take_notes(f"no piece at {capture_pos}")
-
-        # out old players turn
-        old_turn_key = f"turn_{parent.board.current_player.name.lower()}"
-        current_hash ^= cls.ZOBRIST_KEYS[old_turn_key]
-
-        # xor in new state
-        new_pos = move_option.position
-        #* piece state could change (pawn promotion) so use new board for keys
-        piece_new_board = find_piece_at_position(new_board, new_pos)
-        if piece_new_board:
-            new_piece_key = cls._get_piece_key(piece_new_board)
-            current_hash ^= cls.ZOBRIST_KEYS[new_piece_key]
-        else:
-            take_notes(f"no piece at location {new_pos}")
-        
-        # in new player turn
-        new_turn_key = f"turn_{new_board.current_player.name.lower()}"
-        current_hash ^= cls.ZOBRIST_KEYS[new_turn_key]
-
-        # todo keys for en-peasent
-
-        return current_hash
-
-
-    @classmethod
-    def _initialise_zobrist_keys(cls):
-        piece_types = ['King','Queen','Right','Knight','Bishop','Pawn']
+    def _initialise_zobrist_keys(cls, board_size=(5, 5)):
+        """initialise keys for chess fragments default 5x5 grid"""
+        piece_types = ["King", "Queen", "Right", "Knight", "Bishop", "Pawn"]
         players = ["white", "black"]
-        board_size = (5,5)
+        # 5 x 5 x 6 x 2 keys representing all possible combinations for each square on board
         for x in range(board_size[0]):
             for y in range(board_size[1]):
                 for player in players:
                     for piece_type in piece_types:
                         key_name = f"{player}_{piece_type}_{x}_{y}"
-                        cls.ZOBRIST_KEYS[key_name] = getrandbits(64)
+                        cls._zobrist_keys[key_name] = getrandbits(cls._RANDBITS)
+        # unique key for whose turn it is
         for player in players:
-            cls.ZOBRIST_KEYS[f"turn_{player}"] = getrandbits(64)
+            cls._zobrist_keys[f"turn_{player}"] = getrandbits(cls._RANDBITS)
 
-        # todo add for en-peasant
+    @classmethod
+    def _get_piece_key(cls, piece: Piece) -> str:
+        """helper to return consistent key for pieces"""
+        pos = piece.position
+        player = piece.player
+        return f"{player.name.lower()}_{piece.name}_{pos.x}_{pos.y}"
 
-def find_piece_at_position(board, position):
-    for piece in board.get_pieces():
-        if piece.position == position:
-            return piece
-    return None
+    @classmethod
+    def _get_player_turn_key(cls, board: Board) -> str:
+        """helper to return consistent key for boards current player"""
+        player = board.current_player
+        return f"turn_{player.name.lower()}"
+
+    @classmethod
+    def _calculate_zobrist_signature(cls, node: "Node") -> None:
+        """calculate and set nodes signature using xor (^=) operations
+        , incremental calculation if has a parent"""
+        if not node.is_root():
+            # incremental zobrist
+            node.node_signature = cls._calculate_incremental_signature(
+                node, node.parent
+            )
+        else:
+            # calculate new signature
+            board = node.get_board()
+            z_hash = 0
+            for piece in board.get_pieces():
+                key = cls._get_piece_key(piece)
+                try:
+                    z_hash ^= cls._zobrist_keys[key]
+                except KeyError:
+                    take_notes(f"missing key for {key}")
+            z_hash ^= cls._zobrist_keys[cls._get_player_turn_key(board)]
+            node.node_signature = z_hash
+
+    @classmethod
+    def _calculate_incremental_signature(
+        cls,
+        parent: "Node",
+        piece_to_move: Piece,
+        move_opt: MoveOption,
+        new_board: Board,
+    ) -> int:
+        """increments signature. intuition: xor is own inverse
+        use parents hash, xor off and on differences, generate new hash
+        bitwise operations, so should be fast
+        """
+        # xor out piece that moves
+        z_hash = parent.node_signature
+        old_piece_key = cls._get_piece_key(piece_to_move)
+        z_hash ^= cls._zobrist_keys[old_piece_key]
+        # xor out piece if captured
+        for captured_pos in move_opt.captures:
+            if square := new_board[captured_pos]:
+                captured_key = cls._get_piece_key(square.piece)
+                z_hash ^= captured_key
+            else:
+                take_notes(f"no piece at capture location {captured_pos}")
+        # xor out last players turn
+        old_turn_key = cls._get_player_turn_key(parent.board)
+        z_hash ^= old_turn_key
+        # xor in pieces new position
+        new_pos = move_opt.position
+        if square := new_board[new_pos]:
+            new_piece_key = cls._get_piece_key(square.piece)
+            z_hash ^= cls._zobrist_keys[new_piece_key]
+        else:
+            take_notes(f"no piece at new location {new_pos}")
+        # xor in current player
+        current_player_key = cls._get_player_turn_key(new_board)
+        z_hash ^= cls._zobrist_keys[current_player_key]
+        return z_hash
 
 
+    def __init__(self, board, parent=None, move=None, signature: int = None):
+        """initialise new node"""
+        if not Node._zobrist_keys:
+            Node._initialise_zobrist_keys()
+        self._board: Board = board
+        self._move: tuple[Piece, "Move"] = move
+        self.parent: list[Node] = [] if parent is None else [parent]
+        self._children: list[Node] = []
+        self._value: float | None = None
+        self._depth: int = 0 if parent is None else parent.depth + 1
+        self.node_signature: float = (
+            self._calculate_zobrist_signature() if signature is None else signature
+        )
+        # if node signature has not already been seen add new instance
+        if self.node_signature not in Node.transposition_table:
+            Node.transposition_table[self.node_signature] = self
+        # if terminal game ending move
+        if self.is_terminal():
+            if cannot_move(self._board):
+                self.value = inf  # wins game
+            else:
+                self.value = -inf  # looses game
 
-def execute_move_onboard(board, piece, move):
-    """creates a new board state by cloning given board and applying move"""
-    try:
-        temp_board = board.clone()
-        _, temp_piece, temp_move = copy_piece_move(temp_board, piece, move)
-        if temp_piece and temp_move:
-            temp_piece.move(temp_move)
-            try:
-                next(temp_board.turn_iterator)
-            except StopIteration:
-                take_notes("failed to iterate to next player")
-            return temp_board
-        take_notes(f"Error, no piece or move for {piece.name} at {piece.position}")
-        return None
-    except AttributeError as e:
-        take_notes(f"Fatal: {e}")
-        return None
+    def get_board(self):
+        return self._board
+
+    def is_root(self) -> bool:
+        return not self.parent
